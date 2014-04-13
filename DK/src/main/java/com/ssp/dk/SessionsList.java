5337002682fc62daa376ede3bceced914cc4aa1d
IschDef <ssp1997@yahoo.de>
2014-04-13 18:52:35 +0200

    Added initial SessionsList database

--- DK/src/main/java/com/ssp/dk/SessionsList.java
+++ DK/src/main/java/com/ssp/dk/SessionsList.java
@@ -6,6 +6,9 @@
 package com.ssp.dk;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -176,4 +179,50 @@ public class SessionsList {
         // no session with sessionId was found
         return -1;
     }
+
+
+
+    /** Inner class that defines the database contents.
+     *  Implements BaseColumns to include "_ID" and "_COUNT"
+     */
+    public static abstract class SessionsListDbEntry implements BaseColumns {
+        public static final String TABLE_NAME = "sessionslist";
+        public static final String COLUMN_SESSION_NAME  = "sessionname";
+        public static final String COLUMN_SESSION_CREATION_DATE = "sessioncreationdate";
+    }
+
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String INT_TYPE  = " INTEGER";
+    private static final String COMMA_SEP = ",";
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + SessionsListDbEntry.TABLE_NAME + " (" +
+                    SessionsListDbEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT" + COMMA_SEP +
+                    SessionsListDbEntry.COLUMN_SESSION_NAME  + TEXT_TYPE + COMMA_SEP +
+                    SessionsListDbEntry.COLUMN_SESSION_CREATION_DATE + INT_TYPE  +
+                    " )";
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + SessionsListDbEntry.TABLE_NAME;
+
+    public class SessionsListDbHelper extends SQLiteOpenHelper {
+        // If you change the database schema, you must increment the database version.
+        public static final int DATABASE_VERSION = 1;
+        public static final String DATABASE_NAME = "SessionsList.db";
+
+        public SessionsListDbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE_ENTRIES);
+        }
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // This database is only a cache for online data, so its upgrade policy is
+            // to simply to discard the data and start over
+            db.execSQL(SQL_DELETE_ENTRIES);
+            onCreate(db);
+        }
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            onUpgrade(db, oldVersion, newVersion);
+        }
+    }
+
 }
